2bd6bceceac29e5f583b865634c274a08526c103
Nathan Campos <eeepc904@gmail.com>
2013-09-28 20:08:21 -0300

    Started implementing the RPN stuff

---
+++ js/rpn.js
@@ -0,0 +1,54 @@
+// rpn.js
+// RPN stuff.
+
+var rpn_stack = [];
+
+// TODO: Just add a horizontal separator and clean the stack when the user hits the Clear button.
+
+function print_result(value) {
+	$("#log > ul.symbols").append("<li>=</li>");
+	$("#log > ul.numbers").append("<li>" + value + "</li>");
+}
+
+function operate(symbol) {
+	r(Number($("#preview").val()));
+	$("#preview").val("");
+
+	if (symbol === "Enter") {
+		$("#log > ul.symbols").append("<li></li>");
+	} else if (symbol === "+") {
+		$("#log > ul.symbols").append("<li>+</li>");
+		print_result(r("+"));
+	}
+}
+
+function r(input) {
+	if (typeof input === "number") {
+		console.log("Number");
+		push(input);
+		$("#log > ul.numbers").append("<li>" + input + "</li>");
+	} else if (input === "+") {
+		var n = [ 0, 0 ];
+		n[1] = pop();
+		n[0] = pop();
+
+		var result = n[0] + n[1];
+		push(result);
+
+		return result;
+	}
+}
+
+function push(value) {
+	rpn_stack.push(value);
+}
+
+function pop() {
+	var n = rpn_stack.pop();
+
+	if (n === undefined) {
+		console.error("Sorry, not enough numbers in the stack");
+	}
+
+	return n;
+}
